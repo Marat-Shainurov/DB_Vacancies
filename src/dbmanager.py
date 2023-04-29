73ababe33eae276e22aa294e58c29fabdcf0b2f5
Marat-Shainurov <m_shainurov@mail.ru>
2023-04-29 14:52:16 +0300

    create_tables() method has been added to dbmanager.py, creation of the tables has been checked in pgadmin

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -1,17 +1,53 @@
-import requests
 import psycopg2
-import os
+
+from src.utils import get_vacancies
 
 
 class DBManager:
 
-    def __int__(self, db_name):
-        self.db_name = 'DB_Vacancies'
+    def __init__(self, db_name):
+        self.db_name = db_name
 
-    def add_data_to_db(self):
-
+    def create_tables(self):
+        """
+        Создает таблицы <employers>, <vacancies>.
+        """
         connect_db_vacancies = psycopg2.connect(
             host='localhost', database=self.db_name, user='postgres', password='Benzokolon1')
+
+        try:
+            with connect_db_vacancies:
+                with connect_db_vacancies.cursor() as cur:
+                    cur.execute(
+                        "CREATE TABLE employers"
+                        "(company_name varchar(100), company_id int PRIMARY KEY, company_link varchar(255));"
+
+                        "CREATE TABLE vacancies"
+                        "(vacancy_id int PRIMARY KEY, vacancy_name varchar, salary_from int, salary_to int, "
+                        "salary_currency varchar(50),"
+                        "salary_gross int,"
+                        "company_id int, company_name varchar, experience varchar(50), "
+                        "requirement text, responsibility text, "
+                        "CONSTRAINT fk_vacancies_employers FOREIGN KEY(company_id) REFERENCES employers(company_id))"
+                    )
+        finally:
+            connect_db_vacancies.close()
+
+    def add_data_to_db(self):
+        """
+        Заполняет данными таблицы, на основе ответа API HH.ru.
+        """
+        connect_db_vacancies = psycopg2.connect(
+            host='localhost', database=self.db_name, user='postgres', password='Benzokolon1')
+
+        try:
+            with connect_db_vacancies:
+                with connect_db_vacancies.cursor() as cur:
+                    response = get_vacancies()
+                    for element in response:
+                        pass
+        except:
+            pass
 
     def get_companies_and_vacancies_count(self):
         """
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from src.dbmanager import DBManager
 
 # get_request_hh() and get_vacancies() from utils request data from HH api
 # a DBManager class object is created
 # add_data_to_db() method adds data to the DB
 # class methods operate with the data
+
+
+obj = DBManager('DB_Vacancies')
+obj.create_tables()
