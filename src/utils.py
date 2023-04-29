99f8ca4927edcae470542ba6560576fec95fc65e
Marat-Shainurov <m_shainurov@mail.ru>
2023-04-29 17:56:01 +0300

    companies list has been updated, only_with_salary param has been added

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,8 @@ def get_request_hh(page=0):
     Делает запрос к API HH.ru, возвращает ответ в виде коллекции вакансий.
     """
     experience = ('noExperience', 'between1And3', 'between3And6', 'moreThan6')
-    companies_ids = ('1455', '733', '5390761', '4130174', '3649870', '4759060', '9311920', '1740', '84585', '78638')
+    companies_ids = ('1455', '733', '5390761', '4130174', '3649870', '4759060', '9311920', '1740', '84585', '78638',
+                     '3529', '4934')
 
     params = {
         'area': 113,  # индекс страны
@@ -16,7 +17,8 @@ def get_request_hh(page=0):
         'per_page': 100,  # максимально доступное кол-во вакансий на 1 странице
         'order_by': "salary_desc",  # встроенная сортировка по убыванию ответа по ЗП,
         'employer_id': companies_ids,
-        'experience': experience
+        'experience': experience,
+        'only_with_salary': 'true'
     }
     req = requests.get('https://api.hh.ru/vacancies', params)
 
@@ -43,3 +45,5 @@ def add_data_to_file(data):
     """
     with open('vacancies.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+
+add_data_to_file(get_vacancies())
